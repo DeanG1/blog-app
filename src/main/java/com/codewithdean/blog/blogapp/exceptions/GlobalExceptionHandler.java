2d1047b9bd02723fbd719de28889c3ce2b1cf3b8
Dido2002 <didoguzelev@gmail.com>
2024-06-29 21:13:48 +0300

    Request handling with user id not found!

---
+++ src/main/java/com/codewithdean/blog/blogapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.codewithdean.blog.blogapp.exceptions;
+
+import com.codewithdean.blog.blogapp.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@ControllerAdvice
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message,false);
+        return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+}
