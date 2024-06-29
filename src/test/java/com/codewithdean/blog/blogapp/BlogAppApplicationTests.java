cb06fdb5beeb5c452c39a9ed9cd0462fe16195de
Dido2002 <didoguzelev@gmail.com>
2024-06-29 17:57:54 +0300

    Autowiring interface

--- src/test/java/com/codewithdean/blog/blogapp/BlogAppApplicationTests.java
+++ src/test/java/com/codewithdean/blog/blogapp/BlogAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.codewithdean.blog.blogapp;
 
+import com.codewithdean.blog.blogapp.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppApplicationTests {
-
+    @Autowired
+    private UserRepository userRepository;
     @Test
     void contextLoads() {
     }
 
+    public void repoTest(){
+        String className = this.userRepository.getClass().getName();
+        String packName = this.userRepository.getClass().getPackage().getName();
+        System.out.println(className);
+        System.out.println(packName);
+    }
 }
