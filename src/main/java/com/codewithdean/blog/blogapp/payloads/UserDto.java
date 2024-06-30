8f0404832b60edc9b674a6382c9109d526ca2cb9
Dido2002 <didoguzelev@gmail.com>
2024-06-30 11:48:37 +0300

    Added validating data using Bean-validator in spring boot.Created new exceptionHandler for unproperly filled request with data.

--- src/main/java/com/codewithdean/blog/blogapp/payloads/UserDto.java
+++ src/main/java/com/codewithdean/blog/blogapp/payloads/UserDto.java
@@ -1,4 +1,5 @@
 package com.codewithdean.blog.blogapp.payloads;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +9,14 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private int id;
+    @NotEmpty
+    @Size(min = 4, message = "Username must be atleast 4 characters!")
     private String name;
+    @Email(message = "Email address is not valid")
     private String email;
+    @NotEmpty
+    @Size(min = 5, max = 10, message = "Password must be atleast 5 characters and not more than 10 characters")
     private String password;
+    @NotEmpty
     private String about;
 }
