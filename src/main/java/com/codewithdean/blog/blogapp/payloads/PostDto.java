c687ee8cfef99cdbf1bf0a220d5c4a731ffb86ce
Dido2002 <didoguzelev@gmail.com>
2024-07-20 15:10:52 +0300

    So in this pr i created PostDto, PostRepository, PostService, PostServiceImpl & PostController. Then using Postman i created a post request and added our first post

---
+++ src/main/java/com/codewithdean/blog/blogapp/payloads/PostDto.java
@@ -0,0 +1,18 @@
+package com.codewithdean.blog.blogapp.payloads;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostDto {
+    private String title;
+    private String content;
+    private String imageName;
+    private Date addedDate;
+    private CategoryDto category;
+    private UserDto user;
+}
