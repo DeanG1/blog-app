9b2476ddb9452bf207678d8d772557f4105d424f
Dido2002 <didoguzelev@gmail.com>
2024-07-20 12:22:04 +0300

    So in this pr i added new post entity and relationship with users and categories

---
+++ src/main/java/com/codewithdean/blog/blogapp/enities/Post.java
@@ -0,0 +1,24 @@
+package com.codewithdean.blog.blogapp.enities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer postId;
+    @Column(name="post_title", length = 100, nullable = false)
+    private String title;
+    @Column(length = 10000)
+    private String content;
+    private String imageName;
+    private Date addedDate;
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+    @ManyToOne
+    private User user;
+}
