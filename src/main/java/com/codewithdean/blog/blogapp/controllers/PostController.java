92595a614b8244dd5c9d92d052966752ec1b2206
Dido2002 <didoguzelev@gmail.com>
2024-07-21 11:05:35 +0300

    So in this branch I created functionality for fetching/getting all the posts by user id and by category id

--- src/main/java/com/codewithdean/blog/blogapp/controllers/PostController.java
+++ src/main/java/com/codewithdean/blog/blogapp/controllers/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/")
 public class PostController {
@@ -17,5 +19,17 @@ public class PostController {
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId, @PathVariable Integer categoryId) {
         PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
         return new ResponseEntity<>(createPost, HttpStatus.CREATED);
+    }
+    //Get by user
+    @GetMapping("/user/{userId}/posts")
+    public ResponseEntity<List<PostDto>> getPostsByUser(@PathVariable Integer userId){
+        List<PostDto> posts = this.postService.getPostsByUser(userId);
+        return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
+    }
+    //Get by category
+    @GetMapping("/category/{categoryId}/posts")
+    public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable Integer categoryId){
+        List<PostDto> posts = this.postService.getPostsByCategory(categoryId);
+        return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
     }
 }
